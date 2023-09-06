8041a028d653df45587b49a544f04e3122013cc2
Eric Sorensen <Eric.Sorensen@onlinehome.de>
2023-09-06 20:39:24 +0200

    partial Section3

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,9 @@
+using System.Text;
+using API;
 using API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +14,19 @@ builder.Services.AddDbContext<DataContext>(opt =>{
 } );
 
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options => 
+{
+  options.TokenValidationParameters = new TokenValidationParameters
+  {
+    ValidateIssuerSigningKey = true,
+    IssuerSigningKey= new SymmetricSecurityKey(Encoding
+        .UTF8.GetBytes(builder.Configuration["TokenKey"])),
+    ValidateIssuer=false,
+    ValidateAudience=false
+  };
+});
 
 var app = builder.Build();
 
@@ -17,6 +34,9 @@ app.UseCors(builder => builder
 .AllowAnyHeader()
 .AllowAnyMethod()
 .WithOrigins("https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
